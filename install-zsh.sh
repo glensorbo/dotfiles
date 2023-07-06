d4aa198aa89baca674bf00665b5ce3ee8d74dd73
glensorbo <glensorbo@gmail.com>
2023-07-06 10:32:49 +0000

    changed sudp to sudo

--- install-zsh.sh
+++ install-zsh.sh
@@ -7,7 +7,7 @@ echo "Installing zsh"
 sudo apt install zsh -y
 
 echo "Installing exa"
-sudp apt install exa -y
+sudo apt install exa -y
 
 echo "Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
