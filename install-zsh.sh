b45bb69b766568459707e3c7dee2ca865f36fa0f
glensorbo <glensorbo@gmail.com>
2023-07-07 11:04:36 +0200

    split install zsh files

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,30 +1,10 @@
 #!/usr/bin/bash
 
-echo "Updating and install curl & git"
-sudo apt update && sudo apt install curl git -y
+echo "Updating and install curl & exa"
+sudo apt update && sudo apt install curl exa -y
 
 echo "Installing zsh"
 sudo apt install zsh -y
 
-echo "Installing exa"
-sudo apt install exa -y
-
 echo "Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-echo "Getting PowerLevel10k"
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-
-echo "Getting zsh-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-echo "Getting zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-echo "Moving .zshrc to ~"
-mv .zshrc ~/.zshrc
-
-echo "Setting zsh as default shell"
-chsh -s /usr/bin/zsh
-
-echo "All done, log out and log in again to make changes take effect"
