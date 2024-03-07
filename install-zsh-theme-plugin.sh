f2378c1d5a51dc1151022a4b09ec51135490ed55
glensorbo <glensorbo@gmail.com>
2024-03-07 19:49:34 +0100

    fixes zshrc

--- install-zsh-theme-plugin.sh
+++ install-zsh-theme-plugin.sh
@@ -9,8 +9,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 echo "Getting zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo "Moving .zshrc to ~"
-mv .zshrc ~/.zshrc
+echo "Copying .zshrc to ~"
+mv ~/.zshrc ~/.zshrc-bak
+cp zshrc ~/.zshrc
 
 echo "Setting zsh as default shell"
 chsh -s /usr/bin/zsh
