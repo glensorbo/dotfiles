0a6beda9286138ffc8796ee409aacf5ab2853f00
glensorbo <glensorbo@gmail.com>
2024-03-25 23:09:01 +0100

    updates install script

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,10 +1,52 @@
 #!/bin/bash
 
-minVersion=0.9.5
+main ()
+{
+  minVersion=0.9.5
+  
+  if ! command -v nvim &> /dev/null
+  then
+    read -p 'Nvim was not found on your system. Install? (y/N) ' install
+  
+    case "$install" in
+      y|Y) get_nvim
+      ;;
+      n|N) exit 1
+      ;;
+      *) exit 1
+      ;;
+    esac 
+  fi
+
+  echo "Installing dependencies"
+  sudo apt update && sudo apt install curl gcc fuse libfuse2 unzip 
+
+  installedVersion=$(nvim -v | awk '/NVIM/ {print substr($2, 2)}')
+
+  if ! $(printf '%s\n' "$minVersion" "$installedVersion" | sort -C -V)
+  then
+    read -p "You have installed nvim version $installedVersion which is lower than min required version $minVersion. Do you want to upgrade? (y/N) " upgrade
+
+    case "$upgrade" in
+      y|Y) get_nvim
+        ;;
+    esac
+  fi
+
+  read -p "Do you wish to copy nvim config files to your .config folder? (y/N) " copy
+
+  case "$copy" in
+    y|Y) copy_files
+      ;;
+    n|N) exit 1
+      ;;
+    *) exit 1
+      ;;
+  esac
+}
 
 get_nvim ()
 {
-  
   echo "Getting nvim latest"
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 
@@ -13,10 +55,9 @@ get_nvim ()
 
   echo "Moving nvim into /usr/local/bin/nvim"
   sudo mv nvim.appimage /usr/local/bin/nvim
-
 }
 
-backup_files ()
+backup_folder ()
 {
   echo "Backing up your nvim folder"
   mv ~/.config/nvim ~/.config/nvim-bak
@@ -29,7 +70,7 @@ copy_files ()
     read -p "Seems you have an nvim folder already, do you want to back it up? (y/N) " backup
 
     case "$backup" in
-      y|Y) backup_files
+      y|Y) backup_folder
       ;;
     esac
   fi
@@ -38,53 +79,5 @@ copy_files ()
   mkdir -p ~/.config/nvim && cp -r config/* ~/.config/nvim
 }
 
-if ! command -v nvim &> /dev/null
-then
-  read -p 'nvim was not found on your system. Install? (y/N) ' install
-  
-  case "$install" in
-    y|Y) echo "Continuing.."
-    ;;
-    n|N) exit 1
-    ;;
-    *) exit 1
-    ;;
-  esac
-
-  get_nvim
-
-fi
-
-echo "Installing dependencies"
-sudo apt update && sudo apt install curl gcc fuse libfuse2 unzip 
-
-installedVersion=$(nvim -v | awk '/NVIM/ {print substr($2, 2)}')
-
-if ! $(printf '%s\n' "$minVersion" "$installedVersion" | sort -C -V)
-then
-  read -p "You have installed nvim version $installedVersion which is lower than min required version $minVersion. Do you want to upgrade? (y/N) " upgrade
-
-  case "$upgrade" in
-    y|Y) echo "Continuing.."
-    ;;
-    n|N) exit 1
-    ;;
-    *) exit 1
-    ;;
-  esac
-
-  get_nvim
-
-fi
-
-read -p "Do you wish to copy nvim config files to your .config folder? (y/N) " copy
-
-case "$copy" in
-  y|Y) copy_files
-  ;;
-  n|N) exit 1
-  ;;
-  *) exit 1
-  ;;
-esac
+main "$@"; exit
 
