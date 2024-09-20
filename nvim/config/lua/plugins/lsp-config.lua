c045827ef40cc09b48d8e86cd66f7a5a5b75c555
Glen Sørbø <glensorbo@gmail.com>
2024-09-20 10:46:14 +0200

    update nvim config

--- nvim/config/lua/plugins/lsp-config.lua
+++ nvim/config/lua/plugins/lsp-config.lua
@@ -47,7 +47,7 @@ return {
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 		local servers = {
-			tsserver = {},
+			ts_ls = {},
 			lua_ls = {
 				settings = {
 					Lua = {
