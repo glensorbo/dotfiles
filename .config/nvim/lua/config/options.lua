4fac4208de7742bd1b44918bdec64d6e1c5591b1
Glen Sørbø <glensorbo@gmail.com>
2025-05-15 19:32:44 +0200

    newnvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,55 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.scrolloff = 8
-vim.opt.smartindent = true
-
-vim.opt.mouse = "a"
-
-vim.opt.showmode = false
 
 vim.opt.clipboard = "unnamedplus"
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.signcolumn = "yes"
-
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
-vim.opt.inccommand = "split"
-
-vim.opt.cursorline = true
 
 vim.opt.spelllang = "en_gb"
 vim.opt.spell = true
 
-vim.opt.conceallevel = 1
-
-vim.g.snacks_animate = false
-
 vim.g.lazyvim_eslint_auto_format = false
 
 vim.lsp.set_log_level("off")
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,53 +1,28 @@
 return {
   "neovim/nvim-lspconfig",
-  -- other settings removed for brevity
   opts = {
-    ---@type lspconfig.options
     servers = {
-      eslint = {
-        settings = {
-          -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
-          workingDirectories = { mode = "auto" },
-          format = false,
+      omnisharp = {
+        handlers = {
+          ["textDocument/definition"] = function(...)
+            return require("omnisharp_extended").handler(...)
+          end,
         },
+        keys = {
+          {
+            "gd",
+            LazyVim.has("telescope.nvim") and function()
+              require("omnisharp_extended").telescope_lsp_definitions()
+            end or function()
+              require("omnisharp_extended").lsp_definitions()
+            end,
+            desc = "Goto Definition",
+          },
+        },
+        enable_roslyn_analyzers = true,
+        organize_imports_on_format = true,
+        enable_import_completion = true,
       },
-    },
-    inlay_hints = { enabled = false },
-    setup = {
-      eslint = function()
-
-        -- local function get_client(buf)
-        --   return LazyVim.lsp.get_clients({ name = "eslint", bufnr = buf })[1]
-        -- end
-        --
-        -- local formatter = LazyVim.lsp.formatter({
-        --   name = "eslint: lsp",
-        --   primary = false,
-        --   priority = 200,
-        --   filter = "eslint",
-        -- })
-        --
-        -- -- Use EslintFixAll on Neovim < 0.10.0
-        -- if not pcall(require, "vim.lsp._dynamic") then
-        --   formatter.name = "eslint: EslintFixAll"
-        --   formatter.sources = function(buf)
-        --     local client = get_client(buf)
-        --     return client and { "eslint" } or {}
-        --   end
-        --   formatter.format = function(buf)
-        --     local client = get_client(buf)
-        --     if client then
-        --       local diag = vim.diagnostic.get(buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
-        --       if #diag > 0 then
-        --         vim.cmd("EslintFixAll")
-        --       end
-        --     end
-        --   end
-        -- end
-
-        -- register the formatter with LazyVim
-        -- LazyVim.format.register(formatter)
-      end,
     },
   },
 }
