df108f5c18e7c69089d1c574c9bd47e6c9751d97
Glen Sørbø <glensorbo@gmail.com>
2025-05-21 07:59:09 +0200

    neotest-vitest

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -3,11 +3,19 @@ return {
   optional = true,
   dependencies = {
     "Issafalcon/neotest-dotnet",
+    "marilari88/neotest-vitest",
   },
   opts = {
     adapters = {
       ["neotest-dotnet"] = {
         -- Here we can set options for neotest-dotnet
+      },
+      ["neotest-vitest"] = {
+        -- Here we can set options for neotest-vitest
+        vitestCommand = "yarn test",
+        filter_dir = function(name)
+          return name ~= "node_modules"
+        end,
       },
     },
   },
